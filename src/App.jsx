8807a8a64bc826e79b79a724d2ad9edb32a7828e
Tom English <redsoxfantom@gmail.com>
2019-12-08 19:43:38 -0500

    App.jsx updated to swap steps

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,20 @@ import "./App.css";
 import Step1 from './steps/Step1.jsx'
 import Step2 from './steps/Step2.jsx'
 
-class App extends Component{
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      activeStep: Step1
+    }
+  }
+
   render(){
+    let Step = this.state.activeStep;
+
     return(
       <div className="App">
-        <Step1 />
-        <Step2 />
+        <Step />  
       </div>
     );
   }
