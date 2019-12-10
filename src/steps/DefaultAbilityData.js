49d74e92ef8e335f6db33c0892cca620925cfba5
redsoxfantom <redsoxfantom@gmail.com>
2019-12-10 15:04:50 -0500

    Added more abilities

--- src/steps/DefaultAbilityData.js
+++ src/steps/DefaultAbilityData.js
@@ -1,29 +1,160 @@
 const DefaultAbilityData = {
     ACCURACY: {
         value: -2,
-        focuses: [
-            {
-                name: "Bows",
+        focuses: {
+            Bows: {
                 has: false,
-                description: "Proficiency with string-fired weapons, from a handdrawn compound bow to a mechanical crossbow"
+                description: "Proficiency with string-fired weapons, from a handdrawn compound bow to a mechanical crossbow."
             },
-            {
-                name: "Gunnery",
-                has: false
+            Gunnery: {
+                has: false,
+                description: "Proficiency with precision heavy ranged weapons, including ship-mounted weapons."
             },
-            {
-                name: "Pistols",
-                has: false
+            Pistols: {
+                has: false,
+                description: " Proficiency with single-grip, short-barrel ballistic weapons."
             },
-            {
-                name: "Rifles",
-                has: false
+            Rifles: {
+                has: false,
+                description: "Proficiency with double-grip, long-barrel ballistic weapons."
             },
-            {
-                name: "Throwing",
-                has: false
+            Throwing: {
+                has: false,
+                description: "Proficiency with thrown weapons, including grenades."
             },
-        ]
+        }
+    },
+    COMMUNICATION: {
+        value: -2,
+        focuses: {
+            Bargaining: {
+                has: false,
+                description: "Negotiating with others and making deals."
+            },
+            Deception: {
+                has: false,
+                description: "Tricking and lying to other people."
+            },
+            Disguise: {
+                has: false,
+                description: "Changing your appearance to look like someone else."
+            },
+            Etiquette: {
+                has: false,
+                description: "Knowing the social niceties of various cultures."
+            },
+            Expression: {
+                has: false,
+                description: "Conveying thoughts and ideas effectively through words."
+            },
+            Gambling: {
+                has: false,
+                description: "Playing games of chance and profiting from them."
+            },
+            Investigation: {
+                has: false,
+                description: "Interviewing people for information and finding and deciphering clues."
+            },
+            Leadership: {
+                has: false,
+                description: "Guiding, directing, and inspiring others."
+            },
+            Performing: {
+                has: false,
+                description: "Entertaining an audience with a type of performance art"
+            },
+            Persuasion: {
+                has: false,
+                description: "Convincing others to agree with you."
+            },
+            Seduction: {
+                has: false,
+                description: "Getting attention through romantic and sexual allure."
+            }
+        }
+    },
+    CONSTITUTION: {
+        value: -2,
+        focuses: {
+            Running: {
+                has: false,
+                description: "Moving quickly in both short sprints and long distance hauls."
+            },
+            Stamina: {
+                has: false,
+                description: "Enduring fatigue, disease, and privation."
+            },
+            Swimming: {
+                has: false,
+                description: "Moving across water and staying afloat."
+            },
+            Tolerance: {
+                has: false,
+                description: "Withstanding the effects of potentially toxic substances, imbibed for recreation or unwitting exposure."
+            }
+        }
+    },
+    DEXTERITY: {
+        value: -2,
+        focuses: {
+            Acrobatics: {
+                has: false,
+                description: "Executing gymnastic, balancing, and tumbling maneuvers."
+            },
+            Crafting: {
+                has: false,
+                description: "Making things with manual skills, including the various fine and plastic arts. Examples of specific Crafting focuses include Carpentry, Carving, Drawing, Glass-blowing, Leather-working, Painting, Origami, Sculpting, and so forth, but feel free to come up with your own."
+            },
+            Driving: {
+                has: false,
+                description: "Handling surface vehicles like cars, trucks, or motorized carts, to name a few."
+            },
+            Free_Fall: {
+                friendlyName: "Free Fall",
+                has: false,
+                description: "Maneuvering and moving while in free-fall, usually in microgravity."
+            },
+            Initiative: {
+                has: false,
+                description: "Reacting quickly under pressure."
+            },
+            Piloting: {
+                has: false,
+                description: "Operating vehicles that move in three dimensions like aircraft, drones, and spaceships."
+            },
+            Slight_Of_Hand: {
+                friendlyName: "Slight Of Hand",
+                has: false,
+                description: "Using sleight of hand and deft movements to trick others, hide things, or pick pockets."
+            },
+            Stealth: {
+                has: false,
+                description: "Moving around without being noticed."
+            }
+        }
+    },
+    FIGHTING: {
+        value: -2,
+        focuses: {
+            Brawling: {
+                has: false,
+                description: "Proficiency in fighting unarmed, from bareknuckle boxing to various styles of martial arts."
+            },
+            Grappling: {
+                has: false,
+                description: " Proficiency in unarmed techniques intended to immobilize and restrain."
+            },
+            Heavy_Weapons: {
+                friendlyName: "Heavy Weapons",
+                has: false,
+                description: "Proficiency with large or heavy close combat weapons like axes, clubs, hammers, and large swords, to name a few."
+            },
+            Light_Weapons: {
+                friendlyName: "Light Weapons",
+                has: false,
+                description: "Proficiency with light close combat weapons like batons, knives, light staffs, and swords, among others."
+            }
+        }
     }
 }
 
